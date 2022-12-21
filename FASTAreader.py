767bb90132c0a96fa972886a315ff7e7aeac874e
thomasdelong <120509553+thomasdelong@users.noreply.github.com>
2022-12-20 19:11:26 -0500

    added code for https://rosalind.info/problems/cons/

--- FASTAreader.py
+++ FASTAreader.py
@@ -16,19 +16,66 @@ for line in FASTAfile:
         FASTAdict[FASTAlabel] = ""
     else:
         FASTAdict[FASTAlabel] += line
-#creating an adjacency list of overlapping sequences (overlap length = k)
-k=3
-print("Adacency list:")
-for key1, value1 in FASTAdict.items():
-    for key2, value2 in FASTAdict.items():
-        if value1 == value2:
-            continue
-        if value1[-k:] == value2[:k]:
-            print(key1[1:], key2[1:])
-###  GC content calculator
+
+###GC content calculator
 #find key/value pair with highest gc content
 #GCcontentdict = {key: GCcontent(value) for (key, value) in FASTAdict.items()}
 #maxGCkey = max(GCcontentdict, key=GCcontentdict.get)
 #print ID of sequence with highest GC content
 #print(f'Highest GC content:\n{maxGCkey[1:]}\n{GCcontentdict[maxGCkey]}')
 ###
+
+###creating an adjacency list of overlapping sequences (overlap length = k)
+#k=3
+#print("Adacency list:")
+#for key1, value1 in FASTAdict.items():
+#    for key2, value2 in FASTAdict.items():
+#        if value1 == value2:
+#            continue
+#        if value1[-k:] == value2[:k]:
+#            print(key1[1:], key2[1:])
+###
+
+###generating a consensus string and a profile matrix from a list of sequences of identical length
+matrix = []
+for key, value in FASTAdict.items():
+    matrix.append(list(value))
+A=[]
+C=[]
+G=[]
+T=[]
+place=0
+consensus=''
+for i in range(len(matrix[0])):
+    As = 0
+    Cs = 0
+    Gs = 0
+    Ts = 0
+    for list in matrix:
+        base = list[place]
+        if base == 'A':
+            As += 1
+        elif base == 'C':
+            Cs += 1
+        elif base == 'G':
+            Gs += 1
+        elif base == 'T':
+            Ts += 1
+    A.append(As)
+    C.append(Cs)
+    G.append(Gs)
+    T.append(Ts)
+    if A[place] > C[place] and A[place] > G[place] and A[place] > T[place]:
+        consensus += 'A'
+    elif C[place] > G[place] and C[place] > T[place]:
+        consensus += 'C'
+    elif G[place] > T[place]:
+        consensus += 'G'
+    else:
+        consensus += 'T'
+    place += 1
+print(consensus)
+print('A:',*A)
+print('C:',*C)
+print('G:',*G)
+print('T:',*T)
